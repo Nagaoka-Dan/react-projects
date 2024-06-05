d7d82400eaaed5a3298a5493b17decf20a2cb013
Nagaoka-Dan <`nagaoka.dann@gmail.com`>
2024-06-05 22:36:32 +0900

    first commit

---
+++ project1/my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+    return (
+
+        <
+        div className = 'border-gray-400 border rounded-2xl p-2 m-2 flex justify-around items-center' >
+        <
+        h1 className = "text-3xl font-bold underline" > Hello tailwind css < /h1> <
+        p className = "m-0 text-gray-400 " > tailwindcss < /p> <
+        button className = "bg-gray-300 border-2 p-2 rounded-md hover:text-red-500 " > send < /button>  <
+        /div>
+    );
+}
+
+export default App;
